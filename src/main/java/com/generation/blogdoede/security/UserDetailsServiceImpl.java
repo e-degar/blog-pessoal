d7f59826af36c3be0c5c445abc38f6f3727ea7d0
Edgar Soares <edemarinho@outlook.com>
2022-01-28 14:32:58 -0300

    DEV: Cria a classe UserDetailsServiceImpl no pacote security

---
+++ src/main/java/com/generation/blogdoede/security/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.generation.blogdoede.security;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.generation.blogdoede.model.Usuario;
+import com.generation.blogdoede.repository.UsuarioRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+	
+	@Autowired
+	private UsuarioRepository repository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+		Optional<Usuario> user = repository.findAllByLoginUsuarioContainingIgnoreCase(userName);
+		user.orElseThrow(() -> new UsernameNotFoundException(userName + " não encontrado."));
+		return user.map(UserDetailsImpl::new).get();
+	}
+
+}
