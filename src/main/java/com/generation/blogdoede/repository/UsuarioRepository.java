2ea79c584b2e17a86716e96bd99ee47e18991ca1
Edgar Soares <edemarinho@outlook.com>
2022-01-28 11:51:36 -0300

    DEV: Bug fix na UsuarioRepository

--- src/main/java/com/generation/blogdoede/repository/UsuarioRepository.java
+++ src/main/java/com/generation/blogdoede/repository/UsuarioRepository.java
@@ -5,13 +5,12 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.generation.blogdoede.model.Tema;
 import com.generation.blogdoede.model.Usuario;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 	
-	public List<Tema> findAllByNomeUsuarioContainingIgnoreCase (String nomeUsuario);
+	public List<Usuario> findAllByNomeUsuarioContainingIgnoreCase (String nomeUsuario);
 	
-	public List<Tema> findAllByLoginUsuarioContainingIgnoreCase (String loginUsuario);
+	public List<Usuario> findAllByLoginUsuarioContainingIgnoreCase (String loginUsuario);
 }
