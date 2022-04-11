5f37ed7459deae53d3ab365faea76395d9c7baac
Edgar Soares <edemarinho@outlook.com>
2022-04-11 19:11:30 -0300

    FIX: Correção na model Subject

--- src/main/java/com/generation/blogdoede/domain/model/Subject.java
+++ src/main/java/com/generation/blogdoede/domain/model/Subject.java
@@ -10,6 +10,7 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
