13bbffe4463b1d769ac6a72cac8b4bd30feac37e
Edgar Soares <edemarinho@outlook.com>
2022-04-12 10:51:22 -0300

    DEV: Tratamento das validation constraints do Spring Validator

--- src/main/java/com/generation/blogdoede/controller/CustomExceptionHandler.java
+++ src/main/java/com/generation/blogdoede/controller/CustomExceptionHandler.java
@@ -1,15 +1,21 @@
 package com.generation.blogdoede.controller;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -17,6 +23,13 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @ControllerAdvice
 public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
 
+  @ExceptionHandler(Exception.class)
+    public final ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest request) {
+    Map<String, String> errors = new HashMap<>();
+      errors.put("message", ex.getLocalizedMessage());
+    return new ResponseEntity(errors, HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+  
   @Override
   protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
     Map<String, String> errors = new HashMap<>();
@@ -25,6 +38,17 @@ public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
     errors.put("message", errorMessage);
     return new ResponseEntity(errors, HttpStatus.BAD_REQUEST);
   }
+
+  @ExceptionHandler(ConstraintViolationException.class)
+  ResponseEntity<Object> handleConstraintViolation(ConstraintViolationException e) {
+  Map<String, String> errors = new HashMap<>();
+
+  ConstraintViolation violation = e.getConstraintViolations().stream().findFirst().get();
+  errors.put("message", violation.getMessage());
+
+  return new ResponseEntity(errors, HttpStatus.BAD_REQUEST);
+
+}
   
   // Retorna map com os campos e as mensagens de erro
 /*   @Override
--- src/main/java/com/generation/blogdoede/controller/PostController.java
+++ src/main/java/com/generation/blogdoede/controller/PostController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
